6ff63a5709db2440106ec4b208cc8fa84319b66f
Junjun Zhao <zhaosdfa@qq.com>
2015-11-10 20:17:16 +0800

    add a base login api

---
+++ LoginHandler.java
@@ -0,0 +1,80 @@
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.Headers;
+import java.io.*;
+import java.util.List;
+import java.util.Map;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+
+public class LoginHandler implements HttpHandler {
+
+    private static final String ENCODE = "utf-8";
+
+    private void _handle(HttpExchange httpExchange) throws IOException {
+        String requestMethod = httpExchange.getRequestMethod();
+
+        /*
+        Headers headers = httpExchange.getRequestHeaders();
+        List<String> access = headers.get("Access-Token");
+        String accessToken = null;
+        if (access != null && access.size() > 0) {
+            accessToken = access.get(0);
+        }
+        */
+
+
+        int responseCode = 200;
+
+        String data = ExchangeUtils.getRequestBody(httpExchange, ENCODE);
+
+        JSONObject user = JSONObject.fromObject(data);
+
+        System.out.println("--> " + requestMethod + ", " + httpExchange.getRequestURI().toString()
+                + ", data: " + user.getString("username"));
+
+        String username = user.getString("username");
+        String password = user.getString("password");
+
+        JSONObject response = new JSONObject();
+
+
+        if (username == null || password == null || username.equals("wa")) {
+            response.put("code", "USER_AUTH_FAIL");
+            response.put("message", "用户名或密码错误");
+            responseCode = 403;
+        } else {
+            response.put("user_id", "1");
+            response.put("username", username);
+            response.put("access_token", "abcdabcdlsjdflsjdflsjdfljslfjkjs");
+            responseCode = 200;
+        }
+
+        String responseMsg = response.toString();
+        httpExchange.sendResponseHeaders(responseCode, responseMsg.getBytes().length);
+        OutputStream out = httpExchange.getResponseBody();
+        out.write(responseMsg.getBytes());
+        out.flush();
+        httpExchange.close();
+
+    }
+
+    public void handle(HttpExchange httpExchange) throws IOException {
+        try {
+            _handle(httpExchange);
+        } catch (Exception e) {
+            System.out.println(e);
+            JSONObject response = new JSONObject();
+            response.put("code", "MALFORMED_JSON");
+            response.put("message", "格式错误");
+            String responseMsg = response.toString();
+            httpExchange.sendResponseHeaders(400, responseMsg.getBytes().length);
+            OutputStream out = httpExchange.getResponseBody();
+            out.write(responseMsg.getBytes());
+            out.flush();
+            httpExchange.close();
+        }
+    }
+
+}
--- Server.java
+++ Server.java
@@ -7,58 +7,21 @@ import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) {
-		ServerSocket serverSocket = null;
-		try {
-			serverSocket = new ServerSocket(8080);
-			while (true) {
-				Socket client = serverSocket.accept();
-				new Thread(new Handler(client)).start();
-			}
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			try {
-				if (serverSocket != null)
-					serverSocket.close();
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-			}
-		}
-	}
 
-	private static class Handler implements Runnable {
+        try {
 
-		private Socket socket;
+            MyHttpServer server = new MyHttpServer();
+            server.init(8888, 1000);
 
-		Handler(Socket socket) {
-			this.socket = socket;
-		}
+            server.createContext("/login", new LoginHandler());
 
-		@Override
-		public void run() {
-			try {
-				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				String inputLine = in.readLine();
-				String[] contents = inputLine.split(" ");
-				if ("GET".equals(contents[0]) && "/".equals(contents[1])) {
-					socket.getOutputStream().write("Hello, world!\r\n".getBytes());
-					socket.getOutputStream().flush();
-				}
+            server.start();
 
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-			} finally {
-				if (socket != null) {
-					try {
-						socket.close();
-					} catch (IOException e) {
-						System.out.println(e.getMessage());
-					}
-				}
-
-			}
-		}
+        } catch (IOException e) {
+            System.out.println("Exception: " + e.toString());
+        }
 
 	}
+
 }
 
