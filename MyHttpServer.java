7d7aa996fea719934afa213b03dfc94661b3750d
Junjun Zhao <zhaosdfa@qq.com>
2015-11-10 16:20:21 +0800

    inplement a simple http server

---
+++ MyHttpServer.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.spi.HttpServerProvider;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+public class MyHttpServer {
+
+    private HttpServer server;
+
+    /**
+     * port: listen port
+     * backlog: maxinum number of 
+     */
+    public void init(int port, int backlog) throws IOException {
+		HttpServerProvider provider = HttpServerProvider.provider();
+		server = provider.createHttpServer(new InetSocketAddress(port), backlog); 
+		server.setExecutor(null);
+    }
+
+    public void createContext(String uri, HttpHandler handler) {
+        server.createContext(uri, handler);
+    }
+
+	public void start() {
+		server.start();
+	}
+
+}
