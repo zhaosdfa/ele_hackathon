ca0563d4d0beb8f9466a507969bc9915d019e85f
wanggp3 <wanggp3@mail2.sysu.edu.cn>
2015-11-21 21:00:13 +0800

    set ts

--- src/main/java/com/sunsky/server/LuaScript.java
+++ src/main/java/com/sunsky/server/LuaScript.java
@@ -9,23 +9,23 @@ public class LuaScript {
 	public static String sha3 = null;
 	static {
 		Jedis jedis = null;
-			String Script1 = "local ok = 1; local key_food_stock = 'food_stock:'; " +
-				"if tonumber(redis.call('GET',key_food_stock..KEYS[1])) < tonumber(ARGV[1]) then ok = 0; end;" +
-				"if ok == 1 then redis.call('INCRBY',key_food_stock..KEYS[1],-tonumber(ARGV[1])); return 3; else return 1; end;";
-			String Script2 = "local ok = 1; local key_food_stock = 'food_stock:'; " +
-				"if tonumber(redis.call('GET',key_food_stock..KEYS[1])) < tonumber(ARGV[1]) then ok = 0; end;" +
-				"if tonumber(redis.call('GET',key_food_stock..KEYS[2])) < tonumber(ARGV[2]) then ok = 0; end;" +
-				"if ok == 1 then redis.call('INCRBY',key_food_stock..KEYS[1],-tonumber(ARGV[1])); " +
-				"redis.call('INCRBY',key_food_stock..KEYS[2],-tonumber(ARGV[2])); " +
-				"return 3; else return 1; end;";
-			String Script3 = "local ok = 1; local key_food_stock = 'food_stock:'; " +
-				"if tonumber(redis.call('GET',key_food_stock..KEYS[1])) < tonumber(ARGV[1]) then ok = 0; end;" +
-				"if tonumber(redis.call('GET',key_food_stock..KEYS[2])) < tonumber(ARGV[2]) then ok = 0; end;" +
-				"if tonumber(redis.call('GET',key_food_stock..KEYS[3])) < tonumber(ARGV[3]) then ok = 0; end;" +
-				"if ok == 1 then redis.call('INCRBY',key_food_stock..KEYS[1],-tonumber(ARGV[1])); " +
-				"redis.call('INCRBY',key_food_stock..KEYS[2],-tonumber(ARGV[2])); " +
-				"redis.call('INCRBY',key_food_stock..KEYS[3],-tonumber(ARGV[3])); " +
-				"return 3; else return 1; end;";
+		String Script1 = "local ok = 1; local key_food_stock = 'food_stock:'; " +
+			"if tonumber(redis.call('GET',key_food_stock..KEYS[1])) < tonumber(ARGV[1]) then ok = 0; end;" +
+			"if ok == 1 then redis.call('INCRBY',key_food_stock..KEYS[1],-tonumber(ARGV[1])); return 3; else return 1; end;";
+		String Script2 = "local ok = 1; local key_food_stock = 'food_stock:'; " +
+			"if tonumber(redis.call('GET',key_food_stock..KEYS[1])) < tonumber(ARGV[1]) then ok = 0; end;" +
+			"if tonumber(redis.call('GET',key_food_stock..KEYS[2])) < tonumber(ARGV[2]) then ok = 0; end;" +
+			"if ok == 1 then redis.call('INCRBY',key_food_stock..KEYS[1],-tonumber(ARGV[1])); " +
+			"redis.call('INCRBY',key_food_stock..KEYS[2],-tonumber(ARGV[2])); " +
+			"return 3; else return 1; end;";
+		String Script3 = "local ok = 1; local key_food_stock = 'food_stock:'; " +
+			"if tonumber(redis.call('GET',key_food_stock..KEYS[1])) < tonumber(ARGV[1]) then ok = 0; end;" +
+			"if tonumber(redis.call('GET',key_food_stock..KEYS[2])) < tonumber(ARGV[2]) then ok = 0; end;" +
+			"if tonumber(redis.call('GET',key_food_stock..KEYS[3])) < tonumber(ARGV[3]) then ok = 0; end;" +
+			"if ok == 1 then redis.call('INCRBY',key_food_stock..KEYS[1],-tonumber(ARGV[1])); " +
+			"redis.call('INCRBY',key_food_stock..KEYS[2],-tonumber(ARGV[2])); " +
+			"redis.call('INCRBY',key_food_stock..KEYS[3],-tonumber(ARGV[3])); " +
+			"return 3; else return 1; end;";
 		try {
 			jedis = RedisClient.getResource();
 			sha1 = jedis.scriptLoad(Script1);
